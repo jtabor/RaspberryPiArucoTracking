2ebcbc8ede8b7c1d1cb740743a1a066d1adf0a8c
Josh <josh.r.tabor@gmail.com>
2020-11-12 15:11:14 -0600

    changed generate_tags to match our in-class demo

--- generate_tags.py
+++ generate_tags.py
@@ -11,7 +11,7 @@ aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
 fig = plt.figure()
 nx=3
 ny=2
-startId = 50
+startId = 0
 for i in range(1,nx*ny+1):
 	ax = fig.add_subplot(ny,nx,i);
 	img = aruco.drawMarker(aruco_dict,i+startId,700);
